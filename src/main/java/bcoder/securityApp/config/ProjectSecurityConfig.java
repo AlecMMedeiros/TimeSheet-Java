81f091581d8f2e8c320cc41eecdf4cad5b77efa6
Alecsandro Medeiros <alecmm@outlook.com>
2023-03-22 08:38:27 -0300

    Handling with CORS

--- src/main/java/bcoder/securityApp/config/ProjectSecurityConfig.java
+++ src/main/java/bcoder/securityApp/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package bcoder.securityApp.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -28,7 +33,19 @@ public class ProjectSecurityConfig {
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable()
+    http.cors().configurationSource(new CorsConfigurationSource() {
+          @Override
+          public CorsConfiguration getCorsConfiguration ( HttpServletRequest request ) {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Collections.singletonList("*"));
+            configuration.setAllowedMethods(Collections.singletonList("*"));
+            configuration.setAllowCredentials(true);
+            configuration.setAllowedHeaders(Collections.singletonList("*"));
+            configuration.setMaxAge(3600L);
+            return configuration;
+          }
+        }).and()
+        .csrf().disable()
         .authorizeHttpRequests()
         .requestMatchers(ENDPOINTS_AUTHORIZED).authenticated()
         .requestMatchers(ENDPOINTS_PERMIT_ALL).permitAll()
--- src/main/java/bcoder/securityApp/controller/BalanceController.java
+++ src/main/java/bcoder/securityApp/controller/BalanceController.java
@@ -4,6 +4,7 @@ import bcoder.securityApp.service.BalanceService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class BalanceController {
 
